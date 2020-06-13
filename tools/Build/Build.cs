b184c2bd70484b32f947c337544ed0eec4a430bb
Ed Ball <ed.ball@faithlife.com>
2020-06-13 14:23:59 -0700

    Try fixing documentation branch.

--- tools/Build/Build.cs
+++ tools/Build/Build.cs
@@ -3,18 +3,28 @@ using Faithlife.Build;
 
 internal static class Build
 {
-	public static int Main(string[] args) => BuildRunner.Execute(args, build =>
+	public static int Main(string[] args)
 	{
-		build.AddDotNetTargets(
-			new DotNetBuildSettings
-			{
-				NuGetApiKey = Environment.GetEnvironmentVariable("NUGET_API_KEY"),
-				DocsSettings = new DotNetDocsSettings
+		return BuildRunner.Execute(args, build =>
+		{
+			build.AddDotNetTargets(
+				new DotNetBuildSettings
 				{
-					GitLogin = new GitLoginInfo("ejball", Environment.GetEnvironmentVariable("BUILD_BOT_PASSWORD") ?? ""),
-					GitAuthor = new GitAuthorInfo("ejball", "ejball@gmail.com"),
-					SourceCodeUrl = "https://github.com/ejball/ArgsReading/tree/master/src",
-				},
-			});
-	});
+					NuGetApiKey = Environment.GetEnvironmentVariable("NUGET_API_KEY"),
+					DocsSettings = new DotNetDocsSettings
+					{
+						GitLogin = new GitLoginInfo("ejball", Environment.GetEnvironmentVariable("BUILD_BOT_PASSWORD") ?? ""),
+						GitAuthor = new GitAuthorInfo("ejball", "ejball@gmail.com"),
+						SourceCodeUrl = "https://github.com/ejball/ArgsReading/tree/master/src",
+						GitBranchName = GetGitBranchName(),
+					},
+				});
+		});
+
+		string? GetGitBranchName()
+		{
+			const string prefix = "refs/heads/";
+			return Environment.GetEnvironmentVariable("GITHUB_REF") is string githubRef && githubRef.StartsWith("refs/heads/", StringComparison.Ordinal) ? githubRef.Substring(prefix.Length) : null;
+		}
+	}
 }
